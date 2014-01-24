223d8a844481d3a6fdd7ae1cf5aa12632c5ac0c6
Michele Cullen <mcullen4@my.wctc.edu>
2014-01-24 14:41:08 -0600

    Completion of Lab

--- src/java/class2lab/controller/NameDateController.java
+++ src/java/class2lab/controller/NameDateController.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package class2lab.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -76,26 +71,21 @@ private final static String RESULT_PAGE = "javajsp.jsp";
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //processRequest(request, response);
+        
          response.setContentType("text/html");
 
-        // parameters are name attributes in view pages
-        // Here we're retrieving form content from form.html
+        
         String name = request.getParameter("name");
         
-        // Create a new instance of a model object
-        // For some applications, we would not want to create a new one each time.
+        
         WelcomeService welcome = new WelcomeService();
-        // Always a good idea to trim and/or validate input data
+        
         String message = welcome.getWelcomeMessage(name.trim());
 
-        // Parameters are read only Request object properties, but attributes
-        // are read/write. We can use attributes to store data for use on
-        // another page.
+       
         request.setAttribute("message", message);
         
-        // This object lets you forward both the request and response
-        // objects to a destination page
+        
         RequestDispatcher view =
                 request.getRequestDispatcher(RESULT_PAGE);
         view.forward(request, response);
